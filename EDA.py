e07dc44c086548df4e555a95bae3d09c9e708425
No1. 0.6above <cmsg_hbg2@naver.com>
2025-01-30 11:09:00 +0900

    eda

--- EDA.py
+++ EDA.py
@@ -1,10 +1,10 @@
 import pandas as pd
 ####################### 테이블 기본 정보 #######################
-df=pd.read_csv("")
-print(f"shape : {df.shape}") # 행,열 갯수보기
-print(f"columns : {df.columns}") # 열 목록
-print(f"describe() : {pd.describe(df)}") # 평균, 중위값, 표준편차, 25%, 50%, 75%
-print(f"head() : {df.head()}")
+df=pd.read_csv("/data/ephemeral/home/baseline/data/train.csv")
+print(f"shape: {df.shape}") # 행,열 갯수보기
+print(f"columns: {df.columns}") # 열 목록
+print(f"describe(): {df.describe()}") # 평균, 중위값, 표준편차, 25%, 50%, 75%
+print(f"head(): {df.head(3)}")
 ####################### null 갯수 확인 #######################
 df_null=df.isnull().sum()
 df_not_null=df.notnull().sum()
@@ -15,7 +15,7 @@ df_table=pd.DataFrame({
     'full_number' : df_cnt })
 print(df_table)
 ####################### 중복행 찾기 #######################
-print(f"모든 중복 행 수 : {len(df[df.duplicated()])}") # 중복행 keep='first'기본값. 두번째 값만 True로 반환.
+print(f"모든 중복 행 수: {len(df[df.duplicated()])}") # 중복행 keep='first'기본값. 두번째 값만 True로 반환.
+print(f"중복 아닌 행 갯수: {len(df[~df.duplicated()])}")
 print(f"모든 중복 행 수 두번째만: {df.duplicated(subset=df.columns,keep=False).sum()}") # 첫번째 행은 두고, 두번째 행만 센다. 모든 열을 기준으로 하나라도 중복된 행 개수를 반환
-print(f"열별 두번째 이상 중복 수 : {df.apply(lambda col: col.duplicated().sum())}") # 각 열별 중복 개수
-print(f"중복 아닌 행 갯수 : {len(df[~df.duplicated()])}") 
+print(f"열별 두번째 이상 중복 수: \n{df.apply(lambda col: col.duplicated().sum())}") # 각 열별 중복 개수
