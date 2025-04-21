e1aee2f3cd99b5c621f8558ddbf6a00fbeda6f88
DanielinLove <cmsg_hbg2@naver.com>
2025-04-21 17:30:46 +0900

    Python Library

--- MISC_folder/C/heung_c01.c
+++ MISC_folder/C/heung_c01.c
@@ -1,8 +1,14 @@
-﻿// gcc heung_c01.c -o heung_c01
-
-#include <stdio.h>
-int main (){
-    scanf("%d%d", 3, 4);
-    printf("%d", 3 + 4);
-    return 0;
+﻿int mystFunc(int x, int y){
+if(x<2){
+x=y-x;
+return x;
+}else{
+    int nX=x-2;
+    int nY=y-x;
+    int ret= mystFunc(nX, nY);
+    printf("%d, %d, %d, %d\n", x, y, nX, nY);
+    return ret + x;
+    }
 }
+int result = mystFunc(5, 14);
+printf("%d\n",result);
---
+++ Python_test/test.py
@@ -0,0 +1,8 @@
+﻿def test(num):
+    if num<2:
+        print(num, end='')
+    else:
+        test(num//2)
+        print(num%2, end='')
+test(20)
+        
