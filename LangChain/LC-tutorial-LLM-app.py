53ebffac08f14fdc2667807ea859a3b360fab06d
DanielinLove <cmsg_hbg2@naver.com>
2025-07-03 09:52:23 +0900

    langchain_jeon

--- LangChain/LC-tutorial-LLM-app.py
+++ LangChain/LC-tutorial-LLM-app.py
@@ -2,9 +2,12 @@
 
 from dotenv import load_dotenv; import os
 load_dotenv(dotenv_path="C:/code-samples/.env", override=True)
-# print(os.getenv("OPENAI_API_KEY"))
       
 from langchain.chat_models import init_chat_model
 model = init_chat_model("gpt-4o-mini", model_provider="openai")
-res=model.invoke("Hello, world!")
+      
+from langchain_core.messages import HumanMessage, SystemMessage
+messages = [SystemMessage("You are one of a kind, a single, kind and welcoming frined of mine"),HumanMessage("What are you doing?")]
+
+res=model.invoke(messages)
 print(res.content)
